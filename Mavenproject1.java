125461cfca4b7ed3385f4c3e4da5e233fb7f1f29
gusthas <119694981+gusthas@users.noreply.github.com>
2023-02-21 15:07:56 -0300

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,46 @@
+
+ import java.util.Scanner;
+ 
+ /**
+  *
+  * @author Home
+  */
+ public class Mavenproject1 {
+ 
+     public static void main(String[] args) {
+        Scanner cal= new Scanner(System.in);
+        System.out.println("digite o primeiro valor");
+        float A= cal.nextFloat();
+        System.out.println("Digite o segundo valor");
+        float B=cal.nextFloat();
+        System.out.println("Digite a operação:\n + \n - \n * \n /");
+        String  C=cal.next();
+        if(null == C){
+            System.out.println("operação invalida");
+        }else switch (C) {
+             case "+":
+                 {
+                     float D= A+B;
+                     System.out.print("O resultado é:"+ D);
+                     break;
+                 }
+             case "-":
+                 {
+                     float D= A-B;
+                     System.out.print("O resultado é:"+D);
+                     break;
+                 }
+             case "*":
+                 {
+                     float D= A*B;
+                     System.out.println("O resultado é:"+ D);
+                     break;
+                 }
+             default:
+                 System.out.println("operação invalida");
+                 break;
+         }
+ 
+     }
+ }
+ 
